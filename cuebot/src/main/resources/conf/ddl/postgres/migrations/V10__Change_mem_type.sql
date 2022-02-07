56f16df6c0963676aa44c5c88242d593476d16d3
jkellefiel4 <98527483+jkellefiel4@users.noreply.github.com>
2022-02-06 21:51:49 -0300

    Update V10__Change_mem_type.sql

--- cuebot/src/main/resources/conf/ddl/postgres/migrations/V10__Change_mem_type.sql
+++ cuebot/src/main/resources/conf/ddl/postgres/migrations/V10__Change_mem_type.sql
@@ -1,6 +1,6 @@
 -- Change memory type from INT to BIGINT.
 
-ALTER TABLE "job_mem" ALTER COLUMN "int_max_rss" TYPE BIGINT;
+ALTER TABLE "job_mem" ALTER COLUMN "BIGINT_max_rss" TYPE BIGINT;
 ALTER TABLE "job_mem" ALTER COLUMN "int_max_vss" TYPE BIGINT;
 ALTER TABLE "job_resource" ALTER COLUMN "int_max_rss" TYPE BIGINT;
 ALTER TABLE "job_resource" ALTER COLUMN "int_max_vss" TYPE BIGINT;
